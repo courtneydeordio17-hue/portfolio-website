e67450d716f82cbb652394614a910ba63532e8d9
courtneydeordio17-hue <courtneydeordio17@gmail.com>
2025-10-21 11:42:20 -0600

    created scripts file and added code to index.html

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+alert("Hello");
+var age = 31;
+console.log (age);
+var name = 'Courtney';
+console.log (name);
+var size = 100;
+var maxsize = (size * 4) + 15;
+console.log ("Hi there! Welcome to my Portfolio");
